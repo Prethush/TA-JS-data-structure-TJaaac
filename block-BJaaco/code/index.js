ec0b57bf07d484d9fbcb3810396103ecbe187bcf
prethush <ktmirash49@gmail.com>
2021-04-11 17:00:17 +0530

    objects and arrays

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,11 +3,30 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
+let collection = [];
+
+for(let number of numbers) {
+  collection.push(number);
+}
+for(let userId of userIds) {
+  collection.push(userId);
+}
 
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
+let evenCollection = [];
+for(let num of collection) {
+  if(num % 2 === 0) {
+    evenCollection.push(num);
+  }
+}
 
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
-
+let oddCollection = [];
+for(let num of collection) {
+  if(num % 2 !== 0) {
+    oddCollection.push(num);
+  }
+}
 /*
   @param means parameter
 
@@ -24,8 +43,12 @@ let userIds = [1230, 234, 1278, 984, 763, 900];
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
 
-function times() {
-  // Your code
+function times(num, str = "test") {
+  let arr = [];
+  for(let i = 0; i < num; i++) {
+    arr.push(str);
+  }
+  return arr;
 }
 
 // Uncomment the code below and test the output
@@ -48,8 +71,12 @@ function times() {
     revert(['Ryan', 'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function revert() {
-  // your code
+function revert(arr1) {
+  let arr2 = [];
+  for(let i = arr1.length -1; i >= 0; i--) {
+    arr2.push(arr1[i]);
+  }
+  return arr2;
 }
 
 // Uncomment the code below and test the output
@@ -70,8 +97,14 @@ function revert() {
     clear(['Ryan', null, 0,  'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function clear() {
-  // your code
+function clear(arr1) {
+  let arr2 = [];
+  for(let i = arr1.length - 1; i >= 0; i--) {
+    if(arr1[i] !== "" && arr1[i] !== undefined && arr1[i] !== 0 && arr1[i] !== null && arr1[i] !== false) {
+      arr2.push(arr1[i]);
+    }
+  }
+  return arr2;
 }
 
 // Uncomment the code below and test the output
@@ -93,8 +126,12 @@ function clear() {
     arrayToObj(['Ryan', 'John']); // {0: 'Ryan', 1: 'John'}
 */
 
-function arrayToObj() {
-  // your code
+function arrayToObj(arr) {
+  let obj = {};
+  for(let i = 0; i < arr.length; i++) {
+    obj[i] = arr[i];
+  }
+  return obj;
 }
 
 // Uncomment the code below and test the output
